0868eda9affc451d9822cb8513496bb42074b876
OrcBat <orcun.bat@noseryoung.com>
2023-07-07 15:10:03 +0200

    permission evaluator and docker compose

--- m347backend/src/main/java/com/example/demo/core/security/permissionevaluators/UserPermissionEvaluator.java
+++ m347backend/src/main/java/com/example/demo/core/security/permissionevaluators/UserPermissionEvaluator.java
@@ -3,6 +3,8 @@ package com.example.demo.core.security.permissionevaluators;
 import com.example.demo.domain.user.User;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class UserPermissionEvaluator {
 
@@ -11,6 +13,10 @@ public class UserPermissionEvaluator {
 
   public boolean isUserAboveAge(User principal, int age) {
     return true;
+  }
+
+  public boolean isOwnProfile(User principal, UUID id) {
+    return principal.getId().equals(id);
   }
 
 }
